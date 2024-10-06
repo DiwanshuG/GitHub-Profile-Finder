71df277f5256ce00ec9bb080ce1d16db7872b56d
Diwanshu Gangwar <118631453+DiwanshuG@users.noreply.github.com>
2024-10-06 18:22:42 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import GithubProfileFinder from "./components/github-profile-finder";
+function App() {
+  return (
+    <div className="App">
+      {/* Github Profile Finder Component  */}
+
+      <GithubProfileFinder/>
+    </div>
+  );
+}
+
+
+export default App;
